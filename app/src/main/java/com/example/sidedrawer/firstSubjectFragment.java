c62bbf8439be89165d7808bfa7297886cd99c621
ipshitag <iuippi3@gmail.com>
2019-08-15 00:03:37 +0530

    update checkbox

--- app/src/main/java/com/example/sidedrawer/firstSubjectFragment.java
+++ app/src/main/java/com/example/sidedrawer/firstSubjectFragment.java
@@ -17,9 +17,9 @@ public class firstSubjectFragment extends Fragment
     public Context mContext;
     View myView;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-    {
-        myView = inflater.inflate(R.layout.subject_one,container,false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        myView = inflater.inflate(R.layout.subject_one, container, false);
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.subject_one, null);
         CheckBox checkBox1 = (CheckBox) root.findViewById(R.id.checkBox1);
         CheckBox checkBox2 = (CheckBox) root.findViewById(R.id.checkbox2);
@@ -29,11 +29,77 @@ public class firstSubjectFragment extends Fragment
         CheckBox checkBox6 = (CheckBox) root.findViewById(R.id.checkbox6);
         CheckBox checkBox7 = (CheckBox) root.findViewById(R.id.checkbox7);
 
+         boolean checked1 = PreferenceManager.getDefaultSharedPreferences(this.getActivity())
+                .getBoolean("checkBox1", false);
+        checkBox1.setChecked(checked1);
 
+        boolean checked2 = PreferenceManager.getDefaultSharedPreferences(this.getActivity())
+                .getBoolean("checkBox1", false);
 
+        boolean checked3 = PreferenceManager.getDefaultSharedPreferences(this.getActivity())
+                .getBoolean("checkBox1", false);
+
+        boolean checked4 = PreferenceManager.getDefaultSharedPreferences(this.getActivity())
+                .getBoolean("checkBox1", false);
+
+        boolean checked5 = PreferenceManager.getDefaultSharedPreferences(this.getActivity())
+                .getBoolean("checkBox1", false);
+
+        boolean checked6 = PreferenceManager.getDefaultSharedPreferences(this.getActivity())
+                .getBoolean("checkBox1", false);
+
+        boolean checked7 = PreferenceManager.getDefaultSharedPreferences(this.getActivity())
+                .getBoolean("checkBox1", false);
 
         return root;
 
     }
 
-}
+    public void onCheckboxClicked(View view)
+    {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+        switch (myView.getId())
+        {
+            case R.id.checkBox1:
+                PreferenceManager.getDefaultSharedPreferences(this.getActivity()).edit()
+                        .putBoolean("checkBox1", checked).commit();
+                break;
+
+            case R.id.checkbox2:
+                PreferenceManager.getDefaultSharedPreferences(this.getActivity()).edit()
+                        .putBoolean("checkBox1", checked).commit();
+                break;
+
+            case R.id.checkbox3:
+                PreferenceManager.getDefaultSharedPreferences(this.getActivity()).edit()
+                        .putBoolean("checkBox1", checked).commit();
+                break;
+
+            case R.id.checkbox4:
+                PreferenceManager.getDefaultSharedPreferences(this.getActivity()).edit()
+                        .putBoolean("checkBox1", checked).commit();
+                break;
+
+            case R.id.checkbox5:
+                PreferenceManager.getDefaultSharedPreferences(this.getActivity()).edit()
+                        .putBoolean("checkBox1", checked).commit();
+                break;
+
+            case R.id.checkbox6:
+                PreferenceManager.getDefaultSharedPreferences(this.getActivity()).edit()
+                        .putBoolean("checkBox1", checked).commit();
+                break;
+
+            case R.id.checkbox7:
+                PreferenceManager.getDefaultSharedPreferences(this.getActivity()).edit()
+                        .putBoolean("checkBox1", checked).commit();
+                break;
+        }
+
+    }
+
+
+    }
+
+
