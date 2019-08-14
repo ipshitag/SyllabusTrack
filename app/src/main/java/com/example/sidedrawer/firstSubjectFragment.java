96c328160b82050f904ed0d854c43d623088490c
ipshitag <iuippi3@gmail.com>
2019-08-14 22:29:42 +0530

    update checkbox

--- app/src/main/java/com/example/sidedrawer/firstSubjectFragment.java
+++ app/src/main/java/com/example/sidedrawer/firstSubjectFragment.java
@@ -1,14 +1,20 @@
 package com.example.sidedrawer;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 
+import static android.content.Context.MODE_PRIVATE;
+
 public class firstSubjectFragment extends Fragment
 {
+    public Context mContext;
     View myView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -16,13 +22,18 @@ public class firstSubjectFragment extends Fragment
         myView = inflater.inflate(R.layout.subject_one,container,false);
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.subject_one, null);
         CheckBox checkBox1 = (CheckBox) root.findViewById(R.id.checkBox1);
-        CheckBox checkBox2 = (CheckBox) root.findViewById(R.id.checkBox1);
-        CheckBox checkBox3 = (CheckBox) root.findViewById(R.id.checkBox1);
-        CheckBox checkBox4 = (CheckBox) root.findViewById(R.id.checkBox1);
-        CheckBox checkBox5 = (CheckBox) root.findViewById(R.id.checkBox1);
-        CheckBox checkBox6 = (CheckBox) root.findViewById(R.id.checkBox1);
-        CheckBox checkBox7 = (CheckBox) root.findViewById(R.id.checkBox1);
+        CheckBox checkBox2 = (CheckBox) root.findViewById(R.id.checkbox2);
+        CheckBox checkBox3 = (CheckBox) root.findViewById(R.id.checkbox3);
+        CheckBox checkBox4 = (CheckBox) root.findViewById(R.id.checkbox4);
+        CheckBox checkBox5 = (CheckBox) root.findViewById(R.id.checkbox5);
+        CheckBox checkBox6 = (CheckBox) root.findViewById(R.id.checkbox6);
+        CheckBox checkBox7 = (CheckBox) root.findViewById(R.id.checkbox7);
+
+
+
+
         return root;
 
     }
+
 }
