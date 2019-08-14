67456edc27a7477957e84139b45ab6544207231c
ipshitag <iuippi3@gmail.com>
2019-08-14 20:16:52 +0530

    update checkbox

--- app/src/main/java/com/example/sidedrawer/firstSubjectFragment.java
+++ app/src/main/java/com/example/sidedrawer/firstSubjectFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 
 public class firstSubjectFragment extends Fragment
 {
@@ -13,6 +14,15 @@ public class firstSubjectFragment extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         myView = inflater.inflate(R.layout.subject_one,container,false);
-        return myView;
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.subject_one, null);
+        CheckBox checkBox1 = (CheckBox) root.findViewById(R.id.checkBox1);
+        CheckBox checkBox2 = (CheckBox) root.findViewById(R.id.checkBox1);
+        CheckBox checkBox3 = (CheckBox) root.findViewById(R.id.checkBox1);
+        CheckBox checkBox4 = (CheckBox) root.findViewById(R.id.checkBox1);
+        CheckBox checkBox5 = (CheckBox) root.findViewById(R.id.checkBox1);
+        CheckBox checkBox6 = (CheckBox) root.findViewById(R.id.checkBox1);
+        CheckBox checkBox7 = (CheckBox) root.findViewById(R.id.checkBox1);
+        return root;
+
     }
 }
